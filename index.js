6f189a6f794cbd5c3f5bdacc2eefe845cde5879d
Kylie <81180052+one-sketch@users.noreply.github.com>
2025-03-01 19:35:56 -0500

    button shop

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ function toggleTaskWindow() {
     let taskWindow = document.getElementById("taskWindow");
     taskWindow.style.display = (taskWindow.style.display === "block") ? "none" : "block";
 }
+
+function openShop(imageSrc) {
+    let shopWindow = document.getElementById("shopWindow");
+    let shopImage = document.getElementById("shopImage");
+    
+    shopImage.src = "images/" + imageSrc; // Set the shop image
+    shopWindow.style.display = "block"; // Show the shop window
+}
+
+function closeShop() {
+    document.getElementById("shopWindow").style.display = "none";
+}
