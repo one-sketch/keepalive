41b80a584b3579edabaca00b32ff73330c955b38
Kylie <81180052+one-sketch@users.noreply.github.com>
2025-03-01 19:02:20 -0500

    main

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function startGame() {
     window.location.href = 'game.html'; 
 }
+
+function toggleTaskWindow() {
+    let taskWindow = document.getElementById("taskWindow");
+    taskWindow.style.display = (taskWindow.style.display === "block") ? "none" : "block";
+}
